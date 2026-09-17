f378846c245466c5d4f918ba8545b11326e0fb3e
Firmansyah diana <firchun025@gmail.com>
2026-09-17 10:24:30 +0900

    fix: relasi user pada ApiToken harus public agar with('user') berfungsi

--- app/Models/ApiToken.php
+++ app/Models/ApiToken.php
@@ -18,7 +18,7 @@ class ApiToken extends Model
         ];
     }
 
-    protected function user():BelongsTo{
+    public function user():BelongsTo{
         return $this->belongsTo(User::class);
     }
 
